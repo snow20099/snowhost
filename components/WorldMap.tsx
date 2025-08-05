a0d97b1998ef4d28c8f8b5e37397234b432fab64
snow20099 <g3748082@gmail.com>
2025-08-05 20:57:27 +0300

    Update WorldMap.tsx

--- components/WorldMap.tsx
+++ components/WorldMap.tsx
@@ -2,6 +2,7 @@
 'use client'
 
 import React from "react"
+import Image from "next/image"
 
 type Dot = {
   top: string
@@ -18,18 +19,20 @@ const glowingDots: Dot[] = [
 
 const WorldMap: React.FC = () => {
   return (
-    <div className="relative w-full h-full bg-black overflow-hidden rounded-lg">
-      {/* إذا أردت إظهار صورة خريطة، فعّل السطر التالي: */}
-      {/
-      <img
+    <div className="relative w-full h-[500px] bg-background overflow-hidden rounded-lg">
+      {/* Subtle background world map SVG */}
+      <Image
         src="/world-map.svg"
         alt="World Map"
-        className="absolute inset-0 w-full h-full object-cover opacity-20 pointer-events-none"
+        fill
+        className="object-cover opacity-20 pointer-events-none"
+        priority
       />
-      /}
 
+      {/* Gradient glow overlay */}
       <div className="absolute inset-0 bg-gradient-to-tr from-blue-900/20 via-black to-indigo-900/20 z-0" />
 
+      {/* Glowing animated dots */}
       {glowingDots.map((dot, index) => (
         <div
           key={index}
@@ -37,9 +40,12 @@ const WorldMap: React.FC = () => {
           style={{ top: dot.top, left: dot.left }}
         >
           <div className="relative">
+            {/* Glow */}
             <span className="absolute inset-0 animate-ping rounded-full bg-cyan-400 opacity-75"></span>
+            {/* Dot */}
             <span className="relative w-4 h-4 bg-cyan-400 rounded-full block shadow-[0_0_15px_4px_rgba(0,255,255,0.5)]" />
           </div>
+          {/* Optional label on hover */}
           {dot.label && (
             <div className="absolute top-6 left-1/2 -translate-x-1/2 px-2 py-1 text-xs text-white bg-black/60 rounded shadow-md opacity-0 group-hover:opacity-100 transition duration-300">
               {dot.label}
