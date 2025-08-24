9411a8761662c99def6537d61a401786cf600d81
snow20099 <g3748082@gmail.com>
2025-08-24 13:21:40 +0300

    Create mongodb.ts

---
+++ lib/mongodb.ts
@@ -0,0 +1,28 @@
+// lib/mongodb.ts
+import mongoose from 'mongoose'
+
+const MONGO_URI = process.env.MONGO_URI || ''
+
+if (!MONGO_URI) {
+  throw new Error('Please define the MONGO_URI environment variable inside .env')
+}
+
+let cached = (global as any).mongoose
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null }
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI).then(mongoose => mongoose)
+  }
+  cached.conn = await cached.promise
+  return cached.conn
+}
+
+export default dbConnect
