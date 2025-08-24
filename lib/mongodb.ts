4f48c039e364a798aba8f16819aefde040264d74
snow20099 <g3748082@gmail.com>
2025-08-24 13:36:06 +0300

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,28 +1,22 @@
-// lib/mongodb.ts
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI || ''
+const MONGODB_URI = process.env.MONGODB_URI!;
 
-if (!MONGO_URI) {
-  throw new Error('Please define the MONGO_URI environment variable inside .env')
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
 }
 
-let cached = (global as any).mongoose
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null }
-}
-
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn
-  }
-
+export async function connectToDatabase() {
+  if (cached.conn) return cached.conn;
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_URI).then(mongoose => mongoose)
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    }).then((mongoose) => mongoose);
   }
-  cached.conn = await cached.promise
-  return cached.conn
-}
+  cached.conn = await cached.promise;
+  (global as any).mongoose = cached;
+  return cached.conn;
 
-export default dbConnect
+}
